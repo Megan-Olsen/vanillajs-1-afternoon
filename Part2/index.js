a599e2b582c91733345a81bf8783daf76fcccd04
Megan Olsen <megan.a.olsen63@gmail.com>
2020-08-12 07:32:13 -0600

    poker done except black diamond

---
+++ Part2/index.js
@@ -0,0 +1,9 @@
+let idInput = document.getElementById('idInput');
+let colorInput = document.getElementById('colorInput');
+ //console.log(idInput);
+ //console.log(colorInput);
+function setCard(){
+    let card = document.getElementById(idInput.value);
+    //console.log(card);
+    card.style.color = colorInput.value;
+}
